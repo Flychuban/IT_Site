26a29fe011909d76e5e74b7b76b88ad1184b504c
Kaloyan Anastasov <kaloanas07@gmail.com>
2022-04-29 19:16:20 +0300

    navbar finished

--- functions.js
+++ functions.js
@@ -3,10 +3,44 @@ const lightButton = document.getElementById('light');
 const solarizedButton = document.getElementById('solarized');
 const body = document.body;
 
+
+const theme = localStorage.getItem('theme');
+if (theme){
+    body.classList.add(theme);
+}
+
 darkButton.onclick = () =>{
-    body.classList.replace('light', 'dark');
+    if (body.classList.contains('light'))
+    {
+        body.classList.replace('light', 'dark');
+    }
+    else if(body.classList.contains('solarized'))
+    {
+        body.classList.replace('solarized', 'dark');
+    }
+    localStorage.setItem('theme', 'dark');
 };
 
 lightButton.onclick = () =>{
-    body.classList.replace('dark', 'light');
+    if (body.classList.contains('dark'))
+    {
+        body.classList.replace('dark', 'light');
+    }
+    else if(body.classList.contains('solarized'))
+    {
+        body.classList.replace('solarized', 'light');
+    }
+    localStorage.setItem('theme', 'light');
+};
+
+solarizedButton.onclick = () =>{
+    if (body.classList.contains('light'))
+    {
+        body.classList.replace('light', 'solarized');
+    }
+    else if(body.classList.contains('dark'))
+    {
+        body.classList.replace('dark', 'solarized');
+    }
+    localStorage.setItem('theme', 'solarized');
 };
