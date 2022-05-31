afc95cc302d1519330405609a15a3d9b76416ccd
Kaloyan Anastasov <kaloanas07@gmail.com>
2022-05-31 20:06:46 +0300

    space shuttle

--- shuttles_functions.js
+++ shuttles_functions.js
@@ -46,21 +46,22 @@ solarizedButton.onclick = () =>{
 };
 
 let text = document.getElementById('text');
-let ufo = document.getElementById('ufo');
-let alien_par = document.getElementById('alien_par');
-let star = document.getElementById('falling_star');
 let header = document.getElementById('header');
+let shuttle_nasa = document.getElementById('shuttle_nasa');
+let satelit = document.getElementById('satelit');
+let station = document.getElementById('station');
+
 
 window.addEventListener('scroll', function()
 {
     let value = window.scrollY;
-
-    text.style.top = value * -0.1 + 50 + '%';
-    ufo.style.top = value * -1.5 + 'px';
-    ufo.style.left = value * 2 + 'px';
-    alien_par.style.top= value * 1.15 + 'px';
-    alien_par.style.left= value * -1.25 + 'px';
-    star.style.top= value * -2 + 'px';
-    star.style.left= value * 0.9 + 'px';
-    header.style.top= value * -0.05 + 'px';
+  
+   text.style.top = value * -0.1 + 50 + '%';
+   header.style.top= value * -0.05 + 'px'; 
+   station.style.top = value * -1.25 + 'px';
+   station.style.left = value * 0.5 + 'px';
+   shuttle_nasa.style.top = value * -3 + 'px';
+   shuttle_nasa.style.left = value * 1.5 + 'px';
+   satelit.style.top = value * 0.75 + 'px';
+   satelit.style.left = value * -1.25 + 'px';
 });
